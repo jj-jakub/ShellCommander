3206f17d3828c7df2e5bcaa8de01c267d36f8f9e
Jakub Jasiński <jakub.jasinski@treelineinteractive.com>
2024-02-29 21:56:05 +0100

    Create S9BattlestavesGESequence

--- src/main/kotlin/data/click/DefaultClickRepository.kt
+++ src/main/kotlin/data/click/DefaultClickRepository.kt
@@ -2,6 +2,7 @@ package data.click
 
 import domain.click.ClickRepository
 import domain.model.basic.BasicSequence
+import domain.sequences.S9BattlestavesGESequence
 import domain.sequences.S9BlastFurnaceMithGoldSequence
 import domain.sequences.S9CanifisRooftopsSequence
 import domain.sequences.S9FaladorRooftopsSequence
@@ -9,5 +10,5 @@ import domain.sequences.S9HerbloreGESequence
 import domain.sequences.S9MLMSequence
 
 class DefaultClickRepository : ClickRepository {
-    override fun getDefaultBasicSequence(): BasicSequence = S9HerbloreGESequence().getSequence()
+    override fun getDefaultBasicSequence(): BasicSequence = S9BattlestavesGESequence().getSequence()
 }
---
+++ src/main/kotlin/domain/sequences/S9BattlestavesGESequence.kt
@@ -0,0 +1,46 @@
+package domain.sequences
+
+import domain.model.basic.BasicSequence
+import domain.model.basic.BasicSequenceEvent
+import domain.model.basic.Pause
+import domain.model.basic.Square
+
+class S9BattlestavesGESequence : ComposedSequence {
+    override fun getSequence() = BasicSequence(
+        repeatTimes = 1,
+        events = mutableListOf<BasicSequenceEvent>().apply {
+            addAll(sequenceMoves)
+        }
+    )
+
+    // Camera zoom = 4/5 dots Direction East
+    private val bankSq = Square(1375.0F, 611.0F, 1540.0F, 676.0F)
+    private val delay1 = Pause(800, 1000)
+    private val emptyEqSq = Square(1765.0F, 1280.0F, 1839.0F, 1356.0F)
+    private val delay2 = Pause(500, 700)
+    private val orbSq = Square(1299.0F, 978.0F, 1360.0F, 1037.0F)
+    private val battlestaffSq = Square(1435.0F, 978.0F, 1493.0F, 1040.0F)
+    private val closeBankSq = Square(1909.0F, 476.0F, 1939.0F, 510.0F)
+    private val delay3 = Pause(500, 700)
+    private val eqOrbSq = Square(2434.0F, 947.0F, 2481.0F, 997.0F)
+    private val eqBattlestaffSq = Square(2555.0F, 943.0F, 2606.0F, 1001.0F)
+    private val delay4 = Pause(700, 900)
+    private val makeWineSq = Square(670.0F, 190.0F, 900.0F, 330.0F)
+    private val makingDelay = Pause(178 * 100, 187 * 100)
+
+    private val sequenceMoves = listOf(
+        bankSq,
+        delay1,
+        emptyEqSq,
+        delay2,
+        orbSq,
+        battlestaffSq,
+        closeBankSq,
+        delay3,
+        eqOrbSq,
+        eqBattlestaffSq,
+        delay4,
+        makeWineSq,
+        makingDelay,
+    )
+}
